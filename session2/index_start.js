9b7b159807b596bf34c52eed0e9ba354177e1fb1
Michael McFarland <gardengrove422@gmail.com>
2018-05-14 19:10:47 -0400

    session 2 complete

--- session2/index_start.js
+++ session2/index_start.js
@@ -4,6 +4,27 @@
 
 function longestWord(sen) {
   // SOLUTION 1 - Return a single longest word
+  // Create filtered array
+  const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g);
+
+  // Sort by length
+  const sorted = wordArr.sort(function(a, b) {
+    return b.length - a.length;
+  });
+
+  // If multiple words, put into arrray
+  const longestWordArr = sorted.filter(function(word) {
+    return word.length === sorted[0].length;
+  });
+
+  // Check if more than one array value
+  if(longestWordArr.length === 1) {
+    // Return the word
+    return longestWordArr[0];
+  } else {
+    return longestWordArr;
+  }
+
   // SOLUTION 2 - Return an array and include multiple words if they have the same length
   // SOLUTION 3 - Only return an array if multiple words, otherwise return a string
 }
@@ -13,29 +34,105 @@ function longestWord(sen) {
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
 
-function chunkArray(arr, len) {}
+function chunkArray(arr, len) {
+  // SOLUTION 1
+
+  // // Init chunked array
+  // const chunkedArr = [];
+  // // Set index
+  // let i = 0;
+  //
+  // // Loop While index is less than the array length
+  // while(i < arr.length) {
+  //   // Slice out from the index to the index + the chunk lengh and push on to the chunked array
+  //   chunkedArr.push(arr.slice(i, i + len));
+  //   // Increment by chunk length
+  //   i += len;
+  // }
+  //
+  // return chunkedArr;
+
+  // SOLUTION 2
+
+  // Init chunked arr
+  const chunkedArr = [];
+
+  // Loop through arr
+  arr.forEach(function(val) {
+    // Get last element
+    const last = chunkedArr[chunkedArr.length - 1];
+
+    // Check if last and if last length is equal to the chunk len
+    if(!last || last.length === len) {
+      chunkedArr.push([val]);
+    } else {
+      last.push(val);
+    }
+  });
+
+  return chunkedArr;
+}
 
 // CHALLENGE 3: FLATTEN ARRAY
 // Take an array of arrays and flatten to a single array
 // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
 
-function flattenArray(arrays) {}
+function flattenArray(arrays) {
+  // SOLUTION 1
+  // return arrays.reduce(function(a, b) {
+  //   return a.concat(b);
+  // });
+
+  // SOLUTION 2
+  // return [].concat.apply([], arrays);
+
+  // SOLUTION 3
+  function add(a,b,c) {
+    return a + b + c;
+  }
+}
 
 // CHALLENGE 4: ANAGRAM
 // Return true if anagram and false if not
 // ex. 'elbow' === 'below'
 // ex. 'Dormitory' === 'dirty room##'
 
-function isAnagram(str1, str2) {}
+function isAnagram(str1, str2) {
+  return formatStr(str1) === formatStr(str2);
+}
+
+// Helper function
+function formatStr(str) {
+  return str
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort()
+    .join('')
+}
 
 // CHALLENGE 5: LETTER CHANGES
 // Change every letter of the string to the one that follows it and capitalize the vowels
 // Z should turn to A
 // ex. 'hello there' === 'Ifmmp UIfsf'
 
-function letterChanges(str) {}
+function letterChanges(str) {
+  let newStr = str.toLowerCase().replace(/[a-z]/gi, function(char) {
+    if(char === 'z' || char === 'Z') {
+      return 'a';
+    } else {
+      return String.fromCharCode(char.charCodeAt() + 1);
+    }
+  });
+
+  newStr = newStr.replace(/a|e|i|o|u/gi, function(vowel) {
+    return vowel.toUpperCase();
+  });
+
+  return newStr;
+}
 
 // Call Function
-const output = longestWord('Hello, my name is Brad');
+const output = chunkArray([1, 2, 3, 4, 5, 6, 7], 3);
 
 console.log(output);
