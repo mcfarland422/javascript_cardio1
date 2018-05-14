7f1e0975c5689100b1f36dfef09f1977288930e5
Michael McFarland <gardengrove422@gmail.com>
2018-05-14 13:05:29 -0400

    fourth

--- session1/index_start.js
+++ session1/index_start.js
@@ -49,14 +49,28 @@ function reverseInt(int) {
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
 function capitalizeLetters(str) {
-  const strArr = str.toLowerCase().split(' ');
+  // const strArr = str.toLowerCase().split(' ');
+  //
+  // for(let i = 0; i < strArr.length; i++) {
+  //   strArr[i] = strArr[i].substring(0, 1).toUpperCase() +
+  //   strArr[i].substring(1);
+  // }
+  //
+  // return strArr.join(' '); // join back to a string seperated by spaces
 
-  for(let i = 0; i < strArr.length; i++) {
-    strArr[i] = strArr[i].substring(0, 1).toUpperCase() +
-    strArr[i].substring(1);
-  }
+  ///////////////////////
 
-  return strArr.join(' '); // join back to a string seperated by spaces
+  // return str
+  // .toLowerCase()
+  // .split(' ')
+  // .map(function(word) {
+  //   return word[0].toUpperCase() + word.substr(1);
+  // })
+  // .join(' ');
+
+  return str.replace(/\b[a-z]/gi, function(char) {
+    return char.toUpperCase();
+  });
 }
 
 
@@ -64,17 +78,41 @@ function capitalizeLetters(str) {
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) {}
+function maxCharacter(str) {
+  const charMap = {};
+
+  str.split('').forEach(function(char) {
+    if(charMap[char]) {
+      charMap[char]++;;
+    } else {
+      charMap[char] = 1;
+    }
+  });
+
+  console.log(charMap);
+}
 
 
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+function fizzBuzz() {
+  for(let i = 1; i <= 100; i++) {
+    if( i % 3 === 0 && i % 5 ===0) {
+      console.log('FizzBuzz');
+    } else if(i % 3 === 0) {
+      console.log('Fizz');
+    } else if(i % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
 
 
 
 // Call Function
-const output = capitalizeLetters('i love javascript');
+const output = fizzBuzz();
 
 console.log(output);
